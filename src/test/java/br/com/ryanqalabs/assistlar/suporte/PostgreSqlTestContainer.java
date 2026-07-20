f9f7672f7873f8abfa791e34806e5661c6104d3e
CarlosRyan07 <carlosryan.profissional@gmail.com>
2026-07-20 17:15:00 -0300

    fix: compartilha PostgreSQL entre testes de integracao

--- src/test/java/br/com/ryanqalabs/assistlar/suporte/PostgreSqlTestContainer.java
+++ src/test/java/br/com/ryanqalabs/assistlar/suporte/PostgreSqlTestContainer.java
@@ -2,18 +2,18 @@ package br.com.ryanqalabs.assistlar.suporte;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
-@Testcontainers
 public abstract class PostgreSqlTestContainer {
 
-    @Container
     protected static final PostgreSQLContainer POSTGRESQL = new PostgreSQLContainer(ImagemPostgreSql.IMAGEM)
             .withDatabaseName("assistlar_testes")
             .withUsername("assistlar")
             .withPassword("assistlar");
+
+    static {
+        POSTGRESQL.start();
+    }
 
     @DynamicPropertySource
     static void configurarBanco(DynamicPropertyRegistry registry) {
